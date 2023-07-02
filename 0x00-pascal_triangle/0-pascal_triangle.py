a72e02ba9948d81e7d8a9de2916ef44e1c70e5fa
Luke David <daiveedlucas049@gmail.com>
2023-07-02 04:19:18 +0000

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+ pascal's triangle
+"""
+def pascal_triangle(n):
+    """
+    function that return a list of lists of integers rep pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        curr_row = [1]
+
+        for j in range(1, i):
+            curr_row.append(prev_row[j - 1] + prev_row[j])
+
+        curr_row.append(1)
+        triangle.append(curr_row)
+
+    return triangle
