21fdf13e2726a0709962e78dd6af6bc6cfc9cf86
Luke David <daiveedlucas049@gmail.com>
2023-07-05 06:25:11 +0000

    :rocket: lock boxes solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    method that determines if all the boxes can be opened
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        curr_box = stack.pop()
+
+        for key in boxes[curr_box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited) or n == 0
