79bb61ceb77bf6c93c3dc5c69beb37af5c824e92
Luke David <daiveedlucas049@gmail.com>
2023-07-21 05:28:45 +0100

    :wrench: fix 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,93 +1,41 @@
 #!/usr/bin/python3
-"""
-A script that reads stdin line by line and computes metrics
-"""
-import re
+import sys
 
-
-def extract_input(input_line):
+def print_stats(total_file_size, status_codes):
     """
-    Extracts sections of a line of an HTTP request log.
+    Function to print the current statistics
     """
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+    print("File size: {}".format(total_file_size))
+    for status_code in sorted(status_codes.keys()):
+        if status_codes[status_code] > 0:
+            print("{}: {}".format(status_code, status_codes[status_code]))
 
-
-def print_statistics(total_file_size, status_codes_stats):
+def main():
     """
-    Prints the accumulated statistics of the HTTP request log.
+    Main function
     """
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
-
-
-def update_metrics(line, total_file_size, status_codes_stats):
-    """
-    Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    """
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
-def run():
-    """
-    Function that starts the log parser.
-    """
-    line_num = 0
     total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     try:
-        while True:
-            line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
-    except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
+        for i, line in enumerate(sys.stdin, 1):
+            # Parse the log line to extract file size and status code
+            parts = line.split()
+            if len(parts) >= 7:
+                status_code = parts[-2]
+                file_size = int(parts[-1])
 
+                # Update statistics
+                total_file_size += file_size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
 
-if __name__ == '__main__':
-    run()
+            # Print statistics after every 10 lines
+            if i % 10 == 0:
+                print_stats(total_file_size, status_codes)
+
+    except KeyboardInterrupt:
+        # Print the statistics when a keyboard interrupt (CTRL+C) is received
+        print_stats(total_file_size, status_codes)
+
+if __name__ == "__main__":
+    main()
