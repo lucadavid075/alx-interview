5374843e85de581dc689f0bd1b4e363479c859c3
Luke David <daiveedlucas049@gmail.com>
2023-08-18 02:43:12 +0000

    :sparkles: 2D matrix 90degress rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Function for rotating 2D Matrix 90 degrees clockwise
+    """
+
+    n = len(matrix[0])
+
+    for i in range(n - 1, -1, -1):
+        for j in range(0, n):
+            matrix[j].append(matrix[i].pop(0))
